a08e38d0dfe9b14a433c23279ec4c4605f69d459
Gustavo Inacio <incio.gusta@gmail.com>
2019-04-11 11:22:39 -0300

    Fixed some errors and added breadth search

--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+import sys
+
 class Vertice:
 	def __init__(self, id, rotulo):
 		self.id = id
@@ -14,10 +16,10 @@ class Vertice:
 		return self.rotulo
 
 	def peso(self, vizinho):
-		if haAresta(self, vizinho):
+		if vizinho in self.adjascentes.keys():
 			return self.adjascentes[vizinho]
 		else:
-			return float("inf") 
+			return float("inf")
 			# float("inf") vai criar um número infinito
 
 	def adicionaAresta(self, vizinho, peso):
@@ -35,13 +37,16 @@ class Graph:
 
 		for i in range(1, n+1):
 			id, rotulo = f1[i].split()
-			print(id, rotulo)
+			#print(id, rotulo)
 			self.adicionaVertice(int(id), rotulo)
 
 		for line in f1[n+2:]:
 			v1, v2, peso = line.split()
-			print(v1, v2, peso)
+			#print(v1, v2, peso)
 			self.adicionaAresta(int(v1), int(v2), peso)
+
+	def getVertice(self, id):
+		return self.vertices[id]
 
 	def qtdVertices(self):
 		return len(self.vertices)
@@ -54,9 +59,56 @@ class Graph:
 		self.vertices[id] = novo
 
 	def adicionaAresta(self, v1, v2, peso):
-		self.vertices[v1].adicionaAresta(self.vertices[v2], peso)
-		self.vertices[v2].adicionaAresta(self.vertices[v1], peso)
+		self.vertices[v1].adicionaAresta(v2, peso)
+		self.vertices[v2].adicionaAresta(v1, peso)
 		self.arestas += 1
 
 	def haAresta(self, v1, v2):
-		return self.vertices[v1] in self.vertices[v2].adjascentes and self.vertices[v2] in self.vertices[v1].adjascentes
+		return v1 in self.vertices[v2].adjascentes and v2 in self.vertices[v1].adjascentes
+
+def buscaLargura(g, indice):
+	visited = {}
+	distance = {}
+	predecessor = {}
+	for v in g.vertices.keys():
+		visited[v] = False
+		distance[v] = float("inf")
+		predecessor[v] = None
+
+	visited[indice] = True
+	distance[indice] = 0
+
+	q = []
+	q.append(indice)
+	while len(q) > 0:
+		u = q.pop(0)
+		for n in g.getVertice(u).vizinhos():
+			if not visited[n]:
+				visited[n] = True
+				distance[n] = distance[u] + 1
+				predecessor[n] = u
+				q.append(n)
+	return (distance, predecessor)
+
+
+def main():
+	fileName = sys.argv[1]
+	g = Graph(fileName)
+	n = int(sys.argv[2])
+	if n > g.qtdVertices() or n <= 0:
+		print("O n tem que ser entre [1-" + str(g.qtdVertices()) + "]")
+		return
+	distance, predecessor = buscaLargura(g, n)
+	level = {}
+	for v in g.vertices.keys():
+		if not distance[v] in level:
+			level[distance[v]] = []
+		level[distance[v]].append(v)
+	for k in level.keys():
+		print(str(k) + ": " + ", ".join(map(str, level[k])))
+
+if __name__ == "__main__":
+	if (len(sys.argv) < 3):
+		print("Use: ./" +sys.argv[0] + " [Nome do Arquivo] [N° Vertice]")
+	else:
+		main()
