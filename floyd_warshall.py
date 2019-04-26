e978a0c11fdf35f0604b8e1f1e638fcdb782157c
Paloma Cione <paloma_zankely@hotmail.com>
2019-04-26 16:43:11 -0300

    Adds Floyd-Warshall algorithm

---
+++ floyd_warshall.py
@@ -0,0 +1,47 @@
+import sys
+from graph import *
+from pprint import pprint
+
+def floyd_warshall(g):
+
+	n_matriz = g.qtdVertices()
+	m = [[0 for j in range(n_matriz)]
+		  for i in range(n_matriz)]
+
+
+	for v1 in g.vertices.values():
+		for v2 in g.vertices.values():
+			if (v1 == v2):
+				continue
+			else:
+				m[v1.id - 1][v2.id - 1] = float(v1.peso(v2.id))
+
+	for line in m:
+		print(line)
+
+	print(" ")
+	for k in range(n_matriz):
+	  	for i in range(n_matriz):
+		    for j in range(n_matriz):
+	         	m[i][j] = min(m[i][j], m[i][k]+ m[k][j])
+
+	for index, line in enumerate(m):
+		m[index] = list(map(int,line))
+
+
+	v = 1
+	for line in m:
+			print(str(v) + ":" " "+ str(line).replace('[', '').replace(']', ''))
+			v+=1
+
+                                
+def main():
+	fileName = sys.argv[1]
+	g = Graph(fileName)
+	floyd_warshall(g)
+
+if __name__ == "__main__":
+	if (len(sys.argv) < 2):
+		print("Use: ./" +sys.argv[0] + " [Nome do Arquivo]")
+	else:
+		main()
