7dd52d117f587fe53d5f91b025b24510671af2ab
Gustavo Inacio <incio.gusta@gmail.com>
2019-05-30 03:26:30 -0300

    Fully working Strongly Connected Components

--- fortemente_conexo.py
+++ fortemente_conexo.py
@@ -2,7 +2,7 @@ from graph import *
 
 
 def fortemente_conexas(g):
-    (visited, tempo_i, predecessor, tempo_f) = dfs(g)
+    tempo_f = dfs(g)
 
     transpose_g = Graph()
 
@@ -15,80 +15,58 @@ def fortemente_conexas(g):
         for adj in u.vizinhos():
             transpose_g.get_vertice(adj).adiciona_arco(v, u.peso(adj))
 
-    (visited_t, tempo_i_t, predecessor_t, tempo_f_t) = dfs(transpose_g, tempo_f)
+    floresta = get_strongly_connected_components(transpose_g, tempo_f)
 
-    return predecessor_t
+    return floresta
 
 
-def dfs(g, f=None):
-    visited = {}
-    tempo_i = {}
-    tempo_f = {}
-    predecessor = {}
-    for v in g.vertices.keys():
-        visited[v] = False
-        tempo_i[v] = float("inf")
-        tempo_f[v] = float("inf")
-        predecessor[v] = None
-
-    tempo = 0
+def get_strongly_connected_components(g, tempo_f):
     verticelist = []
-    if f is None:
-        verticelist = g.vertices.keys()
-    else:
-        for idVertice, tempoVertice in f.items():
-            verticelist.append((idVertice, tempoVertice))
-        verticelist.sort(key=lambda x: x[1], reverse=False)
-        verticelist = list(map(lambda x: x[0], verticelist))
+    for idVertice, tempoVertice in tempo_f.items():
+        verticelist.append((idVertice, tempoVertice))
+    verticelist.sort(key=lambda x: x[1], reverse=True)
+    verticelist = list(map(lambda x: x[0], verticelist))
+    visited = {x: False for x in g.vertices}
+    tempo_f = {x: float("inf") for x in g.vertices}
+    tempo = [0]
+    floresta = []
+    for v in verticelist:
+        if not visited[v]:
+            found = []
+            dfs_visit(g, v, visited, tempo_f, found, tempo)
+            if len(found) > 0:
+                floresta.append(found)
+    return floresta
+
+def dfs(g):
+    visited = {x: False for x in g.vertices}
+    tempo_f = {x: float("inf") for x in g.vertices}
+    tempo = [0]
+    verticelist = g.vertices.keys()
     for u in verticelist:
         if not visited[u]:
-            dfs_visit(g, u, visited, tempo_i, predecessor, tempo_f, tempo)
+            dfs_visit(g, u, visited, tempo_f, [], tempo)
+    return tempo_f
 
-    return visited, tempo_i, predecessor, tempo_f
 
-
-def dfs_visit(g, v, visited, tempo_i, predecessor, tempo_f, tempo):
+def dfs_visit(g, v, visited, tempo_f, found, tempo):
     visited[v] = True
-    tempo += 1
-    tempo_i[v] = tempo
+    found.append(v)
+    tempo[0] += 1
     for u in g.get_vertice(v).vizinhos():
         if not visited[u]:
-            predecessor[u] = v
-            dfs_visit(g, u, visited, tempo_i, predecessor, tempo_f, tempo)
-
-    tempo += 1
-    tempo_f[v] = tempo
+            dfs_visit(g, u, visited, tempo_f, found, tempo)
+    tempo[0] += 1
+    tempo_f[v] = tempo[0]
 
 
 def main():
     filename = sys.argv[1]
     g = Graph(filename)
 
-    predecessor_t = fortemente_conexas(g)
-    print(predecessor_t)
-    floresta = {}
-    root = {}
-    for arv in predecessor_t.keys():
-        raiz = None
-        temp = arv
-        if temp in root:
-            continue
-        arvores = set()
-        while temp is not None:
-            if temp in root:
-                raiz = root[temp]
-                break
-            arvores.add(temp)
-            raiz = temp
-            temp = predecessor_t[temp]
-        for node in arvores:
-            root[node] = raiz
-        if raiz not in floresta:
-            floresta[raiz] = set()
-        floresta[raiz].update(arvores)
+    floresta = fortemente_conexas(g)
+    print(floresta)
 
-    for values in floresta.values():
-        print(values)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
