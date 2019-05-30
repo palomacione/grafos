4023d8ad12177a40a5c9bd888ae359071f1d58ef
Gustavo Inacio <incio.gusta@gmail.com>
2019-05-30 03:31:39 -0300

    Using the same function for both dfs

--- fortemente_conexo.py
+++ fortemente_conexo.py
@@ -2,7 +2,7 @@ from graph import *
 
 
 def fortemente_conexas(g):
-    tempo_f = dfs(g)
+    (floresta1, tempo_f) = dfs(g)
 
     transpose_g = Graph()
 
@@ -15,38 +15,31 @@ def fortemente_conexas(g):
         for adj in u.vizinhos():
             transpose_g.get_vertice(adj).adiciona_arco(v, u.peso(adj))
 
-    floresta = get_strongly_connected_components(transpose_g, tempo_f)
+    floresta, tempo_f_t = dfs(transpose_g, tempo_f)
 
     return floresta
 
 
-def get_strongly_connected_components(g, tempo_f):
-    verticelist = []
-    for idVertice, tempoVertice in tempo_f.items():
-        verticelist.append((idVertice, tempoVertice))
-    verticelist.sort(key=lambda x: x[1], reverse=True)
-    verticelist = list(map(lambda x: x[0], verticelist))
+def dfs(g, f=None):
     visited = {x: False for x in g.vertices}
     tempo_f = {x: float("inf") for x in g.vertices}
     tempo = [0]
     floresta = []
-    for v in verticelist:
-        if not visited[v]:
-            found = []
-            dfs_visit(g, v, visited, tempo_f, found, tempo)
-            if len(found) > 0:
-                floresta.append(found)
-    return floresta
-
-def dfs(g):
-    visited = {x: False for x in g.vertices}
-    tempo_f = {x: float("inf") for x in g.vertices}
-    tempo = [0]
-    verticelist = g.vertices.keys()
+    verticelist = []
+    if f is not None:
+        for idVertice, tempoVertice in f.items():
+            verticelist.append((idVertice, tempoVertice))
+        verticelist.sort(key=lambda x: x[1], reverse=True)
+        verticelist = list(map(lambda x: x[0], verticelist))
+    else:
+        verticelist = g.vertices.keys()
     for u in verticelist:
         if not visited[u]:
-            dfs_visit(g, u, visited, tempo_f, [], tempo)
-    return tempo_f
+            found = []
+            dfs_visit(g, u, visited, tempo_f, found, tempo)
+            if len(found) > 0:
+                floresta.append(found)
+    return floresta, tempo_f
 
 
 def dfs_visit(g, v, visited, tempo_f, found, tempo):
