ed6c7eb1f7898d86cddae1f83b4fb7b5d1559894
Gustavo Inacio <incio.gusta@gmail.com>
2019-07-04 15:43:42 -0300

    Added BipartiteGraph

--- graph.py
+++ graph.py
@@ -19,11 +19,11 @@ class Vertice:
     def get_rotulo(self):
         return self.rotulo
 
-    def peso(self, vizinho, fluxo= False):
+    def peso(self, vizinho, fluxo=False):
         if vizinho in self.adjascentes.keys():
             return self.adjascentes[vizinho]
         elif fluxo:
-        	return 0
+            return 0
         else:
             return float("inf")
         # float("inf") vai criar um número infinito
@@ -36,49 +36,28 @@ class Graph:
     def __init__(self, file=None):
         self.vertices = {}
         self.arestas = 0
-        self.s = 0
-        self.t = 0
-
         if file is not None:
-       	    ext = file.split('.')
-            print(ext)
-            f = open(file, "r", encoding='utf8')
-            f1 = f.readlines()
-            if ext[1] == "net":
-	            n = int(f1[0].split()[-1])
+            self.read_file(file)
 
-	            for i in range(1, n + 1):
-	                id_, rotulo = shlex.split(f1[i])
-	                self.adiciona_vertice(int(id_), rotulo)
-	            op = f1[n + 1].split()[0]
-	            for line in f1[n + 2:]:
-	                v1, v2, peso = line.split()
-	                # print(v1, v2, peso)
-	                if op == "*arcs":
-	                    self.get_vertice(v1).adiciona_arco(int(v2), peso)
-	                else:
-	                    self.adiciona_aresta(int(v1), int(v2), peso)
-            else:
-	            for line in f1:
-	                a, v1, v2, peso = line.split()
-	                if line[0] == "c" or line[0] == "p":
-	                    continue;
-	                elif line[0] == "n":
-	                    i = line.split()
-	                    if i[2] == "s":
-	                        self.s = i[1]
-	                    else:
-	                      	self.t = i[1]
-	                elif line[0] == "a":
-	                    self.adiciona_vertice(int(v1), v1)
-	                    self.adiciona_vertice(int(v2), v2)
-	                    self.get_vertice(v1).adiciona_arco(int(v2), peso)
-	                else:
-	                	self.adiciona_vertice(int(v1), v1)
-	                	self.adiciona_vertice(int(v2), v2)
-	                	self.adiciona_aresta(v1, v2, peso)
+    def read_file(self, file):
+        ext = file.split('.')
+        print(ext)
+        f = open(file, "r", encoding='utf8')
+        f1 = f.readlines()
+        if ext[-1] == "net":
+            n = int(f1[0].split()[-1])
 
-
+            for i in range(1, n + 1):
+                id_, rotulo = shlex.split(f1[i])
+                self.adiciona_vertice(int(id_), rotulo)
+            op = f1[n + 1].split()[0]
+            for line in f1[n + 2:]:
+                v1, v2, peso = line.split()
+                # print(v1, v2, peso)
+                if op == "*arcs":
+                    self.get_vertice(v1).adiciona_arco(int(v2), peso)
+                else:
+                    self.adiciona_aresta(int(v1), int(v2), peso)
 
     def get_vertice(self, id_):
         return self.vertices[int(id_)]
@@ -102,6 +81,62 @@ class Graph:
 
     def ha_aresta(self, v1, v2):
         return v1 in self.vertices[v2].adjascentes and v2 in self.vertices[v1].adjascentes
+
+
+class BipartiteGraph(Graph):
+
+    def __init__(self, *args, **kwargs):
+        self.verticesX = set()
+        self.verticesY = set()
+        super(BipartiteGraph, self).__init__(*args, **kwargs)
+
+    def read_file(self, file):
+        ext = file.split('.')
+        f = open(file, "r", encoding='utf8')
+        f1 = f.readlines()
+        if ext[-1] == "gr":
+            for line in f1:
+                a, v1, v2 = line.split()
+                if line[0] == "c" or line[0] == "p":
+                    continue;
+                elif line[0] == "e":
+                    self.adiciona_vertice(int(v1), v1)
+                    self.verticesX.add(int(v1))
+                    self.adiciona_vertice(int(v2), v2)
+                    self.verticesY.add(int(v2))
+                    self.adiciona_aresta(int(v1), int(v2), 1)
+
+
+class FluxoGraph(Graph):
+
+    def __init__(self, *args, **kwargs):
+        self.s = 0
+        self.t = 0
+        super(FluxoGraph, self).__init__(*args, **kwargs)
+
+    def read_file(self, file):
+        ext = file.split('.')
+        f = open(file, "r", encoding='utf8')
+        f1 = f.readlines()
+        if ext[-1] == "gr":
+            for line in f1:
+                a, v1, v2, peso = line.split()
+                if line[0] == "c" or line[0] == "p":
+                    continue;
+                elif line[0] == "n":
+                    i = line.split()
+                    if i[2] == "s":
+                        self.s = i[1]
+                    else:
+                        self.t = i[1]
+                elif line[0] == "a":
+                    self.adiciona_vertice(int(v1), v1)
+                    self.adiciona_vertice(int(v2), v2)
+                    self.get_vertice(v1).adiciona_arco(int(v2), peso)
+                else:
+                    self.adiciona_vertice(int(v1), v1)
+                    self.adiciona_vertice(int(v2), v2)
+                    self.adiciona_aresta(v1, v2, peso)
 
 
 if __name__ == "__main__":
